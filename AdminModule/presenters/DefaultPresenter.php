0c9436c429d39644f02d8630fa56be88cbb6dcf1
Josef Kříž <pepakriz@gmail.com>
2012-02-07 10:18:42 +0100

    first commit

---
+++ AdminModule/presenters/DefaultPresenter.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the Venne:CMS (https://github.com/Venne)
+ *
+ * Copyright (c) 2011 Josef Kříž (pepakriz
+ * @gmail.com)
+ *
+ * For the full copyright and license information, please view
+ * the file license.txt that was distributed with this source code.
+ */
+
+namespace App\KcfinderModule\AdminModule;
+
+use Venne;
+
+/**
+ * @author Josef Kříž <pepakriz@gmail.com>
+ *
+ * @secured
+ */
+class DefaultPresenter extends \Venne\Application\UI\AdminPresenter {
+
+
+
+
+}
---
+++ Module.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of the Venne:CMS (https://github.com/Venne)
+ *
+ * Copyright (c) 2011 Josef Kříž (pepakriz
+ * @gmail.com)
+ *
+ * For the full copyright and license information, please view
+ * the file license.txt that was distributed with this source code.
+ */
+
+namespace App\KcfinderModule;
+
+use Nette\DI\Container;
+
+/**
+ * @author Josef Kříž <pepakriz@gmail.com>
+ */
+class Module extends \Venne\Module\BaseModule {
+
+
+	/** @var string */
+	protected $description = "Filemanager for editors";
+
+	/** @var string */
+	protected $version = "2.0";
+
+}
